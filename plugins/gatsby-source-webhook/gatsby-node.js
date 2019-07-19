6fd846af64bc85973e5d21d728eb211eaf2044ca
Dustin Schau <dustinschau@gmail.com>
2019-07-19 10:59:38 -0700

    chore: use reporter instead

--- plugins/gatsby-source-webhook/gatsby-node.js
+++ plugins/gatsby-source-webhook/gatsby-node.js
@@ -13,10 +13,10 @@ const createNode = (title, actions) => {
   return node
 }
 
-exports.sourceNodes = async function sourceNodes({ actions, createNodeId, createContentDigest, reporter, webhookBody, ...rest }) {
+exports.sourceNodes = async function sourceNodes({ actions, createNodeId, createContentDigest, webhookBody, ...rest }) {
   const helpers = { createNode: actions.createNode, createNodeId, createContentDigest, ...rest }
-  reporter.log(`webhookBody`, webhookBody)
-  reporter.log(rest)
+  console.log(`webhookBody`, webhookBody)
+  console.log(rest)
   if (webhookBody && webhookBody.items) {
     webhookBody.items.forEach(title => createNode(title, helpers))
   } else {
